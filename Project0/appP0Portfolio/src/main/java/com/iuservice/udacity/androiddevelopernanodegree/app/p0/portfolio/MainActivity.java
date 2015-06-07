0ad2c0f429157cb73502b474d020e38d799186d6
Duong Tam Chau <dtc@iuservice.com>
2015-06-07 15:02:39 -0400

    Change package and app name.

--- Project0/appP0Portfolio/src/main/java/com/iuservice/udacity/androiddevelopernanodegree/app/p0/portfolio/MainActivity.java
+++ Project0/appP0Portfolio/src/main/java/com/iuservice/udacity/androiddevelopernanodegree/app/p0/portfolio/MainActivity.java
@@ -1,4 +1,4 @@
-package com.iuservice.udacity.androiddevelopernanodegree.project0;
+package com.iuservice.udacity.androiddevelopernanodegree.app.p0.portfolio;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
