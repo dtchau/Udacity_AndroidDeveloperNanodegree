0d4011b45f1b56799833a1961d9918c2bb7049cb
Duong Tam Chau <dtc@iuservice.com>
2015-06-07 18:39:39 -0400

    Add list view.

--- Project0/appX1Sunshine/src/main/java/com/iuservice/udacity/android/fundamental/app/x1/sunshine/MainActivityFragment.java
+++ Project0/appX1Sunshine/src/main/java/com/iuservice/udacity/android/fundamental/app/x1/sunshine/MainActivityFragment.java
@@ -5,6 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import butterknife.InjectView;
 
 
 /**
@@ -12,12 +18,33 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+  @InjectView(R.id.list_view_forecast)
+  ListView m_forecastListView;
+
   public MainActivityFragment() {
+
   }
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_main, container, false);
+    View myFragment = inflater.inflate(R.layout.fragment_main, container, false);
+    m_forecastListView = (ListView) myFragment.findViewById(R.id.list_view_forecast);
+
+    ArrayList<String> fakeData = new ArrayList<>(50);
+    for (int i = 1; i < 50; ++i) {
+      fakeData.add(String.format("Item #%3d", i));
+    }
+
+    ArrayAdapter<String> weatherAdapter = new ArrayAdapter<String>(
+        getActivity(),
+        R.layout.fake_weather_list_item,
+        R.id.weatherLabel,
+        fakeData
+    );
+
+    m_forecastListView.setAdapter(weatherAdapter);
+
+    return myFragment;
   }
 }
